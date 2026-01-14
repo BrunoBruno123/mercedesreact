4aced81302c9a3d8b6a2243f8ebad1cea64aa743
BrunoBruno123 <bruno_shahu@yahoo.com>
2026-01-14 11:46:09 +0100

    changes

--- src/components/Body/shop.js
+++ src/components/Body/shop.js
@@ -1,13 +1,98 @@
+import car5 from '../../images/car5.png';
+import car8 from '../../images/car8.png';
+import car9 from '../../images/car9.png';
 import { Container } from "react-bootstrap";
+import { Row, Col, Button } from 'react-bootstrap';
 
 const bodyz = () => {
     return (
         <Container>
-            <div>
-                <h1>Shop Page</h1>
-                <h2>Welcome to the Shop!</h2>
-                <h3>Explore our products and offers.</h3>   
-            </div>
+            <Row>
+                <Col xs={12} md={4} lg={4}>
+                    <div className="d-flex justify-content-center mt-4 mb-4">
+                        <img src={car5} alt="G Class" className="rounded-circle w-50"/>
+                    </div>
+                    <h2 className="text-center">
+                        G Class
+                    </h2> 
+                    <p>
+                        G Class e tipit mercedes-benz është një SUV luksoz me katër rrota tërheqëse i prodhuar nga prodhuesi gjerman i makinave Mercedes-Benz.
+                    </p>
+                    <div className="d-flex justify-content-center">
+                        <Button variant="secondary">Order</Button>
+                    </div> 
+                </Col>
+                <Col xs={12} md={4} lg={4}>
+                    <div className="d-flex justify-content-center mt-4 mb-4">
+                        <img src={car8} alt="B Class" className="rounded-circle w-50"/>
+                    </div>
+                    <h2 className="text-center">
+                        B Class
+                    </h2>
+                    <p>
+                        B class e tipit mercedes-benz është një makinë kompakte me pesë dyer me një dizajn monovolumeni të prodhuar nga prodhuesi gjerman i makinave Mercedes-Benz.
+                    </p>
+                    <div className="d-flex justify-content-center">
+                        <Button variant="secondary">Order</Button>
+                    </div>
+                </Col>
+                <Col xs={12} md={4} lg={4}>
+                    <div className="d-flex justify-content-center mt-4 mb-4">
+                        <img src={car9} alt="A Class" className="rounded-circle w-50"/>
+                    </div>
+                    <h2 className="text-center">
+                        A Class
+                    </h2>
+                    <p>
+                        A Class e tipit mercedes-benz është një makinë kompakte me pesë dyer e prodhuar nga prodhuesi gjerman i makinave Mercedes-Benz.
+                    </p>
+                    <div className="d-flex justify-content-center">
+                        <Button variant="secondary">Order</Button>
+                    </div>
+                </Col>
+                <Col xs={12} md={4} lg={4}>
+                    <div className="d-flex justify-content-center mt-4 mb-4">
+                        <img src={car5} alt="G Class" className="rounded-circle w-50"/>
+                    </div>
+                    <h2 className="text-center">
+                        G Class
+                    </h2> 
+                    <p>
+                        G Class e tipit mercedes-benz është një SUV luksoz me katër rrota tërheqëse i prodhuar nga prodhuesi gjerman i makinave Mercedes-Benz.
+                    </p>
+                    <div className="d-flex justify-content-center">
+                        <Button variant="secondary">Order</Button>
+                    </div> 
+                </Col>
+                <Col xs={12} md={4} lg={4}>
+                    <div className="d-flex justify-content-center mt-4 mb-4">
+                        <img src={car8} alt="B Class" className="rounded-circle w-50"/>
+                    </div>
+                    <h2 className="text-center">
+                        B Class
+                    </h2>
+                    <p>
+                        B class e tipit mercedes-benz është një makinë kompakte me pesë dyer me një dizajn monovolumeni të prodhuar nga prodhuesi gjerman i makinave Mercedes-Benz.
+                    </p>
+                    <div className="d-flex justify-content-center">
+                        <Button variant="secondary">Order</Button>
+                    </div>
+                </Col>
+                <Col xs={12} md={4} lg={4}>
+                    <div className="d-flex justify-content-center mt-4 mb-4">
+                        <img src={car9} alt="A Class" className="rounded-circle w-50"/>
+                    </div>
+                    <h2 className="text-center">
+                        A Class
+                    </h2>
+                    <p>
+                        A Class e tipit mercedes-benz është një makinë kompakte me pesë dyer e prodhuar nga prodhuesi gjerman i makinave Mercedes-Benz.
+                    </p>
+                    <div className="d-flex justify-content-center">
+                        <Button variant="secondary">Order</Button>
+                    </div>
+                </Col>
+            </Row>
         </Container>
     );
 }
